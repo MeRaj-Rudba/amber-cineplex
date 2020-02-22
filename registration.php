efd1f77ce7e43a1bb5dad571caf97476eec1d79d
MeRaj Rudba <mrudba@gmail.com>
2020-02-22 15:07:07 +0600

    latest of today

--- registration.php
+++ registration.php
@@ -1,3 +1,26 @@
+<?php
+  $reqUnameErr="";
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <html lang="en" dir="ltr">
   <head>
@@ -24,7 +47,7 @@
 
     </header>
     <main>
-      <div align="center"; class="registrationForm">
+      <div align="center" class="shadowBox">
         <form class="regiFrom" action="index.html" method="post">
           <table>
             <tr>
@@ -33,35 +56,104 @@
                 <h2><span class="highlighter">Amber Cineplex</span> </h2>
               </td>
             </tr>
+            <!--NAME-->
             <tr>
               <td colspan="2">
                 <input class="inputFeild" type="text" name="rgName" placeholder="Your Name">
               </td>
             </tr>
             <tr>
+                    <td>
+                      <div class="error">
+                        <label><?php echo $reqUnameErr; ?></label>
+                      </div>
+                    </td>
+            </tr>
+          <!--DOB-->
+            <tr>
               <td colspan="2">
                 <input class="inputFeild" type="date" name="rgDOB" placeholder="Your Birthday">
               </td>
             </tr>
+            <tr>
+                    <td>
+                      <div class="error">
+                        <label><?php echo $reqUnameErr; ?></label>
+                      </div>
+                    </td>
+            </tr>
+
+          <!--PASSWORD-->
+
             <tr>
               <td colspan="2">
                 <input  class="inputFeild" type="password" name="rgPass" placeholder="Your Password">
               </td>
             </tr>
             <tr>
+                    <td>
+                      <div class="error">
+                        <label><?php echo $reqUnameErr; ?></label>
+                      </div>
+                    </td>
+            </tr>
+
+          <!--CONFIRM PASSWORD-->
+            <tr>
               <td colspan="2">
                 <input  class="inputFeild" type="password" name="rgPassConfirm" placeholder="Confirm Password">
               </td>
             </tr>
+            <tr>
+                    <td>
+                      <div class="error">
+                        <label><?php echo $reqUnameErr; ?></label>
+                      </div>
+                    </td>
+            </tr>
+
+          <!--EMAIL-->
             <tr>
               <td colspan="2">
                 <input class="inputFeild" type="email" name="rgEmail" placeholder="Your Email">
               </td>
             </tr>
             <tr>
+                    <td>
+                      <div class="error">
+                        <label><?php echo $reqUnameErr; ?></label>
+                      </div>
+                    </td>
+            </tr>
+
+          <!--PHONE-->
+            <tr>
               <td colspan="2">
                 <input  class="inputFeild" type="text" name="rgPhone" placeholder="Your Phone Number">
               </td>
+            </tr>
+            <tr>
+                    <td>
+                      <div class="error">
+                        <label><?php echo $reqUnameErr; ?></label>
+                      </div>
+                    </td>
+            </tr>
+            <tr class="row">
+                    <td colspan="2" align="left">
+                      <label class="remember">I agree to the tearms and conditions.
+                        <input type="checkbox" >
+                        <span class="checkmark"></span>
+                      </label>
+                    </td>
+            </tr>
+            <tr class="row">
+                    <td  align="left">
+                      <button type="reset" name="logInButton">SIGN UP</button>
+                    </td>
+                    <td  align="right">
+                      <button type="submit" name="logInButton">RESET</button>
+                    </td>
             </tr>
           </table>
         </form>
