3127799ce21ea0f39a17cd81755d4111d9e4fe9d
MeRaj Rudba <mrudba@gmail.com>
2020-02-23 01:20:31 +0600

    adding home

---
+++ home.php
@@ -0,0 +1,50 @@
+<?php
+ 
+
+
+?>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Josefin+Slab&display=swap" rel="stylesheet">
+    <title>Amber Cineplex | Home</title>
+  </head>
+  <body>
+
+    <header>
+      <div class="container1" >
+        <img src="images/logo4.png" alt="logo" class="logo">
+
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="schedule.php">Schedule</a></li>
+            <li><a href="upcoming.php">Upcoming</a></li>
+            <li><a href="booking.php">Booking</a></li>
+            <li><a href="contact.php">Contact</a></li>
+          </ul>
+        </nav>
+        </div>
+    </header>
+    <main class="homeCenter">
+      <section class="slideShowSection">
+
+      </section>
+      <section class="homeDiscription">
+        <h2>The Biggest Cinplex in Country</h2>
+        <article>
+          <h3>You will see what you want to see</h3>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem nulla fugit minima accusantium, modi obcaecati? Illo, voluptatem molestias accusamus velit voluptatibus officia eligendi quisquam magnam, omnis mollitia aspernatur? Animi, illo.</p>
+        </article>
+      </section>
+
+
+    </main>
+    <footer>
+      <label class="footer">&copy Amber Cineplex</label>
+    </footer>
+
+  </body>
+</html>
